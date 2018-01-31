31197ada082b5ce0d11c2c47ce44ab9831c1576e
zhanglf <251190220@qq.com>
2018-01-31 20:18:42 +0800

    调整代码格式
    简单调整代码格式

--- init.el
+++ init.el
@@ -4,17 +4,31 @@
 
 
 
-(setq inhibit-startup-message t) ;关闭启动画面
 
-(setq column-number-mode t) ;显示列号
-;;; 括号匹配时显示另一个括号而不是跳到另一个括号
+;;关闭启动画面
+(setq inhibit-startup-message t)
 
+;;显示列号
+(setq column-number-mode t)
+
+;; 括号匹配时显示另一个括号而不是跳到另一个括号
 (show-paren-mode t)
+
 (setq show-paren-style 'parentheses)
+
 (setq frame-title-format "%b %I") ;显示文件名和大小
 
-(auto-image-file-mode t) ;让Emacs可以直接打开、显示图片
-(fset 'yes-or-no-p 'y-or-n-p) ;以Y/N代表yes/no
-(setq auto-save-default nil) ;不生成名为#filename#的临时文件
-(setq x-select-enable-clipboard t) ;支持和外部程序的拷贝
-(global-font-lock-mode t) ;打开语法高亮
+;;让Emacs可以直接打开、显示图片
+(auto-image-file-mode t)
+
+;;以Y/N代表yes/no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;不生成名为#filename#的临时文件
+(setq auto-save-default nil) 
+
+;;支持和外部程序的拷贝
+(setq x-select-enable-clipboard t)
+
+;;打开语法高亮
+(global-font-lock-mode t)
