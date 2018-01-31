0c2d8202013919e2400daf175e56898747018212
zhanglf <251190220@qq.com>
2018-01-31 19:21:45 +0800

    增加括号匹配
    括号匹配时显示另外一边的括号，而不是跳到另一个括号。

--- init.el
+++ init.el
@@ -3,3 +3,11 @@
 (scroll-bar-mode 0)
 
 
+
+(setq visible-bell t)
+(setq inhibit-startup-message t)
+(setq column-number-mode t)
+
+;;括号匹配时显示另外一边的括号，而不是跳到另一个括号。
+(show-paren-mode t)
+(setq show-paren-style 'parentheses)
