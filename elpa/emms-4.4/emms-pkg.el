ad4cc0fa321bd83e571ba914256eb21d6c9fbbbe
zhanglf <251190220@qq.com>
2018-02-01 11:08:05 +0800

    提交elpa目录

---
+++ elpa/emms-4.4/emms-pkg.el
@@ -0,0 +1,7 @@
+(define-package "emms" "4.4" "The Emacs Multimedia System"
+  '((cl-lib "0.5"))
+  :keywords
+  '("emms" "mp3" "mpeg" "multimedia"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -1,4 +1,17 @@
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")
+			 ("melpa" . "http://melpa.org/packages/")
+			 ("melpa-stable" . "http://stable.melpa.org/packages/"))
+      package-archive-priorities '(("melpa-stable" . 1))) ;; the package manager
 
+
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;配置use-package包
+(eval-when-compile
+  (require 'use-package))
 
 
 
